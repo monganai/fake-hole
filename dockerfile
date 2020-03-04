74437c519950b1ccd426ee54968223a78185bff3
monganai <monganai@tcd.ie>
2020-03-04 21:32:20 +0000

    first and probably last commit :)

---
+++ dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.7
+
+ENV PYTHONUNBUFFERED 1
+
+COPY . /code/
+WORKDIR /code/
+
+RUN pip install -r requirements.txt
+
+
+CMD [ "python", "./fake-hole.py" ]
+
---
+++ fake-hole.py
@@ -0,0 +1,17 @@
+from flask import Flask, Response
+app = Flask(__name__)
+
+
+@app.route("/pass/admin/api.php")
+def passResponse():
+    dict = '{"domains_being_blocked":125230,"dns_queries_today":13322,"ads_blocked_today":5490,"ads_percentage_today":41.21003,"unique_domains":928,"queries_forwarded":6702,"queries_cached":1130,"clients_ever_seen":4,"unique_clients":4,"dns_queries_all_types":13322,"reply_NODATA":12,"reply_NXDOMAIN":15,"reply_CNAME":173,"reply_IP":317,"privacy_level":0,"status":"enabled"}'
+    return Response(dict, status=200)
+
+@app.route("/fail/admin/api.php")
+def FailResponse():
+    dict = '{"domains_being_blocked":125230,"dns_queries_today":13322,"ads_blocked_today":5490,"ads_percentage_today":41.21003,"unique_domains":928,"queries_forwarded":6702,"queries_cached":1130,"clients_ever_seen":4,"unique_clients":4,"dns_queries_all_types":13322,"reply_NODATA":12,"reply_NXDOMAIN":15,"reply_CNAME":173,"reply_IP":317,"privacy_level":0,"status":"enabled"}'
+    return Response(dict, status=404)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888)
