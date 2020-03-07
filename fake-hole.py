c71296ab34ca26f3ad171f22fe8bad1a86b8bdc9
monganai <monganai@tcd.ie>
2020-03-07 22:19:03 +0000

    two status related endpoints added

--- fake-hole.py
+++ fake-hole.py
@@ -7,6 +7,16 @@ def passResponse():
     dict = '{"domains_being_blocked":125230,"dns_queries_today":13322,"ads_blocked_today":5490,"ads_percentage_today":41.21003,"unique_domains":928,"queries_forwarded":6702,"queries_cached":1130,"clients_ever_seen":4,"unique_clients":4,"dns_queries_all_types":13322,"reply_NODATA":12,"reply_NXDOMAIN":15,"reply_CNAME":173,"reply_IP":317,"privacy_level":0,"status":"enabled"}'
     return Response(dict, status=200)
 
+@app.route("/no_status/admin/api.php")
+def no_status():
+    dict = '{"domains_being_blocked":125230,"dns_queries_today":13322,"ads_blocked_today":5490,"ads_percentage_today":41.21003,"unique_domains":928,"queries_forwarded":6702,"queries_cached":1130,"clients_ever_seen":4,"unique_clients":4,"dns_queries_all_types":13322,"reply_NODATA":12,"reply_NXDOMAIN":15,"reply_CNAME":173,"reply_IP":317,"privacy_level":0,}'
+    return Response(dict, status=200)
+
+@app.route("/bad_status/admin/api.php")
+def bad_status():
+    dict = '{"domains_being_blocked":125230,"dns_queries_today":13322,"ads_blocked_today":5490,"ads_percentage_today":41.21003,"unique_domains":928,"queries_forwarded":6702,"queries_cached":1130,"clients_ever_seen":4,"unique_clients":4,"dns_queries_all_types":13322,"reply_NODATA":12,"reply_NXDOMAIN":15,"reply_CNAME":173,"reply_IP":317,"privacy_level":0,"status":"running"}'
+    return Response(dict, status=200)
+
 @app.route("/fail/admin/api.php")
 def FailResponse():
     dict = '{"domains_being_blocked":125230,"dns_queries_today":13322,"ads_blocked_today":5490,"ads_percentage_today":41.21003,"unique_domains":928,"queries_forwarded":6702,"queries_cached":1130,"clients_ever_seen":4,"unique_clients":4,"dns_queries_all_types":13322,"reply_NODATA":12,"reply_NXDOMAIN":15,"reply_CNAME":173,"reply_IP":317,"privacy_level":0,"status":"enabled"}'
